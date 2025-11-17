da9c71b85c3fbf0fa86750020984700cbf2d7950
Oleg Pyrha <oyurich777@gmail.com>
2025-11-17 19:25:12 +0100

    add styles and renew template

--- single-departments.php
+++ single-departments.php
@@ -9,18 +9,18 @@ get_header();
 ?>
 
 <main id="primary" class="site-main single-departments">
-	<div class="wp-block-group" style="max-width:1100px;margin:0 auto;padding:var(--wp--preset--spacing--50) var(--wp--preset--spacing--30);">
-		<div class="wp-block-columns reverse-mobile">
-			<div class="wp-block-column" style="flex-basis:25%;padding-right:12px;">
+	<div class="wp-block-group single-template-container">
+		<div class="wp-block-columns reverse-mobile single-template-columns">
+			<div class="wp-block-column single-template-sidebar">
 				<?php get_sidebar(); ?>
 			</div>
-			<div class="wp-block-column" style="flex-basis:75%;">
+			<div class="wp-block-column single-template-content">
 				<?php
 				while ( have_posts() ) :
 					the_post();
 					?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class( 'single-department' ); ?>>
-						<header class="entry-header" style="border-left:3px solid var(--wp--preset--color--base);padding-left:16px;margin-bottom:var(--wp--preset--spacing--40);">
+						<header class="entry-header single-template-header">
 							<?php the_title( '<h1 class="entry-title" style="text-transform:uppercase;font-style:normal;font-weight:700;">', '</h1>' ); ?>
 						</header>
 

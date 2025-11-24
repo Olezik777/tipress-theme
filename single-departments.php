08eef131ece0e0abe1e2ba2cdc53098112fd9a0c
Oleg Pyrha <oyurich777@gmail.com>
2025-11-24 09:41:21 +0100

    Add conditional rendering for sidebar and header on departments page based on language selection to improve user experience

--- single-departments.php
+++ single-departments.php
@@ -6,24 +6,32 @@
  */
 
 get_header();
+
+// Определяем текущий язык
+$current_lang = function_exists( 'pll_current_language' ) ? pll_current_language() : 'ru';
+$is_english = ( $current_lang === 'en' );
 ?>
 
 <main id="primary" class="site-main single-departments">
 	<div class="wp-block-group single-template-container">
 		<?php tipress_display_breadcrumbs(); ?>
 		<div class="ti-columns reverse-mobile single-template-columns">
+			<?php if ( ! $is_english ) : ?>
 			<div class="ti-column single-template-sidebar">
 				<?php get_sidebar(); ?>
 			</div>
+			<?php endif; ?>
 			<div class="ti-column single-template-content">
 				<?php
 				while ( have_posts() ) :
 					the_post();
 					?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class( 'single-department' ); ?>>
+						<?php if ( ! $is_english ) : ?>
 						<header class="entry-header single-template-header">
 							<?php the_title( '<h1 class="entry-title" style="text-transform:uppercase;font-style:normal;font-weight:700;">', '</h1>' ); ?>
 						</header>
+						<?php endif; ?>
 
 						<div class="entry-content">
 							<?php the_content(); ?>
